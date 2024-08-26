0032fc6c81a7dd1d217f7ef7d27421729147773d
shanyu-mondem <162614567+shanyu-mondem@users.noreply.github.com>
2024-08-26 11:49:52 +0530

    Add files via upload

---
+++ Program1_PosCount_start_with_1.py
@@ -0,0 +1,10 @@
+# At odd positions if positional count start from 1
+s = input("Enter your string: ")
+chars = list(s)
+for i,char in enumerate(chars):
+    if (i%2==0) and (ord(char) in range(97,123) or ord(char) in range(65,91)):
+        chars[i] = chr(ord(char)-3)
+orig_str=''.join(chars)
+rev_str = orig_str[::-1]
+print('Original String: ',orig_str)
+print("Resultant String: ",rev_str)
